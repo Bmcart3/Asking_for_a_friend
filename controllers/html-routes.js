efefbee0c8f52bb2930da64139f3cee957e298c5
Emily Renegar <egrenegar@gmail.com>
2020-07-23 15:50:41 -0400

    added title and description to all routes for dynamic <meta> tags

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -9,7 +9,11 @@ module.exports = function(app) {
     if (req.user) {
       return res.redirect('/questions'); //was originally members
     }
-    res.render('index');
+    res.locals.metaTags = { 
+      title: 'Asking for a Friend', 
+      description: 'Get answers to the burning questions you\'re too embarrassed to ask irl.'
+    }; 
+    res.render('index', { layout: "main" });
   });
 
   app.get('/login', function(req, res) {
@@ -17,28 +21,52 @@ module.exports = function(app) {
     if (req.user) {
       return res.redirect('/questions');
     }
-    res.render('login');
+    res.locals.metaTags = { 
+      title: 'Login - Asking for a Friend', 
+      description: 'Get answers to the burning questions you\'re too embarrassed to ask irl.'
+    }; 
+    res.render('login', { layout: "main" });
   });
 
   app.get('/signup', function(req, res) {
-    res.render('signup');
+    res.locals.metaTags = { 
+      title: 'Sign Up - Asking for a Friend', 
+      description: 'Get answers to the burning questions you\'re too embarrassed to ask irl.'
+    }; 
+    res.render('signup', { layout: 'main' });
   });
 
   app.get('/index', function(req, res) {
-    res.render('index');
+    res.locals.metaTags = { 
+      title: 'Asking for a Friend', 
+      description: 'Get answers to the burning questions you\'re too embarrassed to ask irl.'
+    };
+    res.render('index', { layout: 'main' });
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get('/questions', isAuthenticated, function(req, res) {
+    
     db.Question.findAll({})
       .then(questions => {
+        res.locals.metaTags = { 
+          title: 'All Questions - Asking for a Friend', 
+          description: 'Ask a new question or answer another user.'
+        };
         res.render('questions', { questions: questions });
       });
   });
 
   app.get('/dashboard', isAuthenticated, function(req, res) {
-    res.render('dashboard');
+    db.Question.findAll({})
+      .then(questions => {
+        res.locals.metaTags = { 
+          title: 'Dashboard - Asking for a Friend', 
+          description: 'Update and delete questions you\'ve asked and answers you\'ve given.'
+        };
+        res.render('dashboard', { questions: questions });
+      });
   });
 
 };
